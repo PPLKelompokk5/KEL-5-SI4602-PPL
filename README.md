7ee90ade0db6003c59eeefd62fefcaf718a26708
ikhwanamrdn <wanmir213@gmail.com>
2025-03-27 20:31:46 +0700

    comit 3 27

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 SETUP
 - set .env
 - composer install
+- php artisan key:generate
 - php artisan migrate
 - php artisan serve
 - buka http://127.0.0.1:8000/admin/login
@@ -8,3 +9,9 @@ SETUP
 SETUP FILAMENT
 - php artisan filament:install --panels
 - php artisan make:filament-user
+
+CARA PULL MASTER TERBARU
+- git stash
+- git checkout master
+- git pull --rebase origin master
+- git stash pop
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Providers\Filament\AdminPanelProvider;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,8 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // Tambahkan middleware jika perlu
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+        // Tambahkan konfigurasi exception jika perlu
+    })
+    ->create();
