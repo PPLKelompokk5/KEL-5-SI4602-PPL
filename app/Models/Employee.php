5165eb9c0eaf5acebb740aba9aabb61b4733f548
ikhwanamrdn <wanmir213@gmail.com>
2025-03-28 19:44:10 +0700

    comit

---
+++ app/Models/Employee.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Employee extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'position_id',
+        'status',
+        'remember_token',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+    public function position()
+    {
+        return $this->belongsTo(Position::class);
+    }
+}
