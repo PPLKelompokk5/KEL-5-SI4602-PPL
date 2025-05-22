a2eaa8e2879e9cd9446d7528e162a2a1077e1948
ikhwanamrdn <wanmir213@gmail.com>
2025-05-22 11:24:01 +0700

    fix 1

--- app/Filament/Admin/Resources/ClientResource/Pages/CreateClient.php
+++ app/Filament/Admin/Resources/ClientResource/Pages/CreateClient.php
@@ -3,10 +3,14 @@
 namespace App\Filament\Admin\Resources\ClientResource\Pages;
 
 use App\Filament\Admin\Resources\ClientResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateClient extends CreateRecord
 {
     protected static string $resource = ClientResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
