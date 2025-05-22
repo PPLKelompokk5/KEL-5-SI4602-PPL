8c6d01e43e8ab16ba23d0ea59405a7d8c3295190
ikhwanamrdn <wanmir213@gmail.com>
2025-05-22 09:19:35 +0700

    landing up

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landing.welcome');
 });
 
 Route::redirect('/admin', '/admin/login')->name('admin.login');
