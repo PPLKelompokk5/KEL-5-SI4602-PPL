97f8eefa8627f555984983d0c60e0d60d69e8a19
ikhwanamrdn <wanmir213@gmail.com>
2025-05-27 14:13:33 +0700

    PRAKK5-43 big update loc

--- app/Filament/Admin/Resources/LocationResource/Pages/EditLocation.php
+++ app/Filament/Admin/Resources/LocationResource/Pages/EditLocation.php
@@ -3,17 +3,18 @@
 namespace App\Filament\Admin\Resources\LocationResource\Pages;
 
 use App\Filament\Admin\Resources\LocationResource;
-use Filament\Actions;
+use App\Models\Project;
 use Filament\Resources\Pages\EditRecord;
 
 class EditLocation extends EditRecord
 {
     protected static string $resource = LocationResource::class;
 
-    protected function getHeaderActions(): array
+    protected function mutateFormDataBeforeSave(array $data): array
     {
-        return [
-            Actions\DeleteAction::make(),
-        ];
+        $project = Project::find($data['project_id']);
+        $data['status'] = ($project && $project->status == 1) ? ($data['status'] ?? 0) : 0;
+
+        return $data;
     }
 }
--- app/Models/Location.php
+++ app/Models/Location.php
@@ -6,10 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Location extends Model
 {
-    protected $fillable = ['project_id', 'name'];
+    protected $fillable = [
+        'project_id',
+        'client_id',
+        'name',
+        'status',
+    ];
 
     public function project()
     {
         return $this->belongsTo(Project::class);
+    }
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
     }
 }
