04a7605c0c3a009ba27b5eed816fcd1948d4accd
ikhwanamrdn <wanmir213@gmail.com>
2025-06-03 14:35:55 +0700

    up rem index

--- app/Filament/Employee/Resources/ReimburstResource/Pages/CreateReimburst.php
+++ app/Filament/Employee/Resources/ReimburstResource/Pages/CreateReimburst.php
@@ -9,4 +9,8 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateReimburst extends CreateRecord
 {
     protected static string $resource = ReimburstResource::class;
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
